9614e406b54c61d37203a4f936a0105b36551071
lvdeshi2011@163.com <lvdeshi2011@gmail.com>
2017-11-10 18:06:22 +0800

    implement part 2

---
+++ app/controllers/admin/jobs_controller.rb
@@ -0,0 +1,44 @@
+class Admin::JobsController < ApplicationController
+    before_action :authenticate_user!,only: [:new,:create,:edit,:update,:destroy]
+    before_action :require_is_admin
+    def index
+        @jobs = Job.all
+    end
+    def  show
+        @job = Job.find(params[:id])
+    end
+    def  new
+        @job = Job.new
+    end
+    def create
+        @job = Job.new(jobs_params)
+        if @job.save
+            redirect_to admin_jobs_path
+        else
+            render :new
+        end
+    end
+
+    def  edit
+        @job = Job.find(params[:id])
+    end
+    def update
+        @job = Job.find(params[:id])
+        @job = Job.update(jobs_params)
+        if @job.save
+            redirect_to admin_jobs_path
+        else
+            render :edit
+        end
+    end
+    def destroy
+        @job = Job.find(params[:id])
+        @job.destroy
+        redirect_to admin_jobs_path
+    end
+
+    private
+    def jobs_params
+        params.require(:job).permit(:title,:description)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :jobs
+    namespace :admin do
+        resources :jobs
+    end
 
-  resources :jobs
 
-
-
-  root 'jobs#index'
+    root 'jobs#index'
 end
