9f49d9768b733caabe109541b08ba1e13216ec3c
lvdeshi2011@163.com <lvdeshi2011@gmail.com>
2017-11-11 01:16:19 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
                 post :publish
                 post :hide
             end
-
+            resources :resumes
         end
     end
     root 'jobs#index'
