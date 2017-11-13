87cc22b8d872ed05612435e9ac236d788c3c56dc
lotus0099 <lvdeshi2011@gmail.com>
2017-11-13 18:14:24 +0800

    fixed navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,16 @@
 //= require bootstrap/dropdown
 //= require bootstrap/collapse
 //= require_tree .
+// $(window).scroll(function () {
+//
+//   if ($(this).scrollTop() > 0) {//当我们下拉超过200的时候
+//     if ($('.nav-s').is(':animated')) {
+//       return false
+//     }
+//     $('.nav-s').addClass('row2_fixed') // 让导航栏固定在顶部
+//     $('.nav-s').stop().animate({top: 0}, 600) // 在600ms内匀速慢慢出来
+//   } else {
+//     $('.nav-s').css({top: -80})
+//     $('.nav-s').removeClass('row2_fixed')
+//   }
+// })
